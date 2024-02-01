96a56ff0ef1e872670ce483d723eecf06a1ff1f3
vnporras <vnporras1028@gmail.com>
2024-02-01 12:58:21 -0800

    modified the .h

--- sort.h
+++ sort.h
@@ -30,4 +30,6 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void custom_quickS(int *array, int low, int high, size_t size);
+int lomuto_partition(int *array, int low, int high, size_t size);
 #endif/*SORT_H*/
